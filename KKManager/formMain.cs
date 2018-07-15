01be9d1ab167e1d6131ea09d44f45059b2e56a24
MarC0 <dummyaccx@gmail.com>
2018-07-15 12:29:32 +0200

    Changed target runtime to 4.6.1; Reference cleanup

--- KKManager/formMain.cs
+++ KKManager/formMain.cs
@@ -3,7 +3,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Windows.Forms;
 using KKManager.Data;
 
