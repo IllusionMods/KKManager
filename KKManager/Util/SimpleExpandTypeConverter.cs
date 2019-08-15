98b00d6b945d511ad7cc09e65b775465f76881d5
MarC0 <dummyaccx@gmail.com>
2019-08-15 17:08:13 +0200

    Fixed card properties being editable and not expanding

---
+++ KKManager/Util/SimpleExpandTypeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace KKManager.Util
+{
+    /// <summary>
+    /// Make the class expandable in property grids
+    /// </summary>
+    /// <typeparam name="T">Type of the class</typeparam>
+    /// <inheritdoc />
+    public sealed class SimpleExpandTypeConverter<T> : TypeConverter
+    {
+        public override PropertyDescriptorCollection GetProperties(ITypeDescriptorContext context, object value, Attribute[] attributes)
+        {
+            return TypeDescriptor.GetProperties(typeof(T), attributes);
+        }
+
+        public override bool GetPropertiesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+    }
+}
