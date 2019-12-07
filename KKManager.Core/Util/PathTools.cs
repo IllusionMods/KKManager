5648f171b8727bf2739c4c0357c2da467cf38b61
MarC0 <dummyaccx@gmail.com>
2019-12-07 16:54:25 +0100

    Changed ftp node getting to be recursive

--- KKManager.Core/Util/PathTools.cs
+++ KKManager.Core/Util/PathTools.cs
@@ -1,10 +1,71 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace KKManager.Util
 {
     public static class PathTools
     {
+        private static readonly char[] PathTrimChars =
+        {
+            '\\',
+            '/',
+            '"',
+            // SPACE 
+            '\u0020',
+            // NO-BREAK SPACE 
+            '\u00A0',
+            // OGHAM SPACE MARK 
+            '\u1680',
+            // EN QUAD 
+            '\u2000',
+            // EM QUAD 
+            '\u2001',
+            // EN SPACE 
+            '\u2002',
+            // EM SPACE 
+            '\u2003',
+            // THREE-PER-EM SPACE 
+            '\u2004',
+            // FOUR-PER-EM SPACE 
+            '\u2005',
+            // SIX-PER-EM SPACE 
+            '\u2006',
+            // FIGURE SPACE 
+            '\u2007',
+            // PUNCTUATION SPACE 
+            '\u2008',
+            // THIN SPACE 
+            '\u2009',
+            // HAIR SPACE 
+            '\u200A',
+            // NARROW NO-BREAK SPACE 
+            '\u202F',
+            // MEDIUM MATHEMATICAL SPACE 
+            '\u205F',
+            // and IDEOGRAPHIC SPACE 
+            '\u3000',
+
+            // LINE SEPARATOR 
+            '\u2028',
+
+            // PARAGRAPH SEPARATOR  
+            '\u2029',
+
+            // CHARACTER TABULATION 
+            '\u0009',
+            // LINE FEED 
+            '\u000A',
+            // LINE TABULATION 
+            '\u000B',
+            // FORM FEED 
+            '\u000C',
+            // CARRIAGE RETURN 
+            '\u000D',
+            // NEXT LINE 
+            '\u0085'
+        };
+
         public static bool DirectoryHasWritePermission(string directoryToTest)
         {
             try
@@ -17,11 +78,49 @@ namespace KKManager.Util
             }
             catch (UnauthorizedAccessException)
             {
-
                 return false;
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// Remove unnecessary spaces, quotes and path separators from start and end of the path.
+        /// Might produce different path than intended in case it contains invalid unicode characters.
+        /// </summary>
+        public static string NormalizePath(string path1)
+        {
+            if (path1 == null) throw new ArgumentNullException(nameof(path1));
+            return path1.Normalize().Trim(PathTrimChars);
+        }
+
+        public static bool PathsEqual(string path1, string path2)
+        {
+            if (string.IsNullOrEmpty(path1) || string.IsNullOrEmpty(path2))
+                return false;
+
+            path1 = NormalizePath(path1).Replace('/', '\\');
+            path2 = NormalizePath(path2).Replace('/', '\\');
+            return path1.Equals(path2, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public static bool PathsEqual(FileSystemInfo path1, FileSystemInfo path2)
+        {
+            if (path1 == null || path2 == null)
+                return false;
+
+            return PathsEqual(path1.FullName, path2.FullName);
+        }
+
+        /// <summary>
+        /// Replace all invalid file name characters from a string with _ so that it can be used as a file name.
+        /// </summary>
+        public static string SanitizeFileName(string name)
+        {
+            var invalidChars = Regex.Escape(new string(Path.GetInvalidFileNameChars()));
+            var invalidRegStr = string.Format(@"([{0}]*\.+$)|([{0}]+)", invalidChars);
+
+            return Regex.Replace(name, invalidRegStr, "_");
         }
     }
 }
