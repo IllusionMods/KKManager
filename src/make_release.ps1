ec9bf636532fc217e40c7d6c5bc069129ce6f777
MarC0 <dummyaccx@gmail.com>
2021-11-07 03:08:37 +0100

    Remove unnecessary build steps; Add release script

---
+++ src/make_release.ps1
@@ -0,0 +1,13 @@
+# Env setup ---------------
+if ($PSScriptRoot -match '.+?\\bin\\?') {
+    $dir = $PSScriptRoot + "\"
+}
+else {
+    $dir = $PSScriptRoot + "\bin\"
+}
+
+$outfile = $dir + "\_release.zip"
+
+Remove-Item $outfile -ErrorAction Ignore -Force
+
+Get-ChildItem $dir -Exclude *.log,*.pdb,*.xml,*.zip,*.7z,*.ps1,*.lnk,*.settings,FileContentsHashCache.bin,UpdateSourcesDeb* | Compress-Archive -DestinationPath $outfile -Force -CompressionLevel Optimal
