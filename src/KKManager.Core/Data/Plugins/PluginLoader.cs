b5bed9b34a92dcf66ffb017810b30307f27191cd
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2022-12-15 10:36:18 +0100

    Add support for patcher plugins

--- src/KKManager.Core/Data/Plugins/PluginLoader.cs
+++ src/KKManager.Core/Data/Plugins/PluginLoader.cs
@@ -16,10 +16,7 @@ namespace KKManager.Data.Plugins
 {
     public static class PluginLoader
     {
-        public static IObservable<PluginInfo> Plugins
-        {
-            get => _plugins ?? StartReload();
-        }
+        public static IObservable<PluginInfo> Plugins => _plugins ?? StartReload();
 
         private static bool _isUpdating = false;
         private static readonly object _lock = new object();
@@ -51,6 +48,7 @@ namespace KKManager.Data.Plugins
         /// Gather information about valid plugins inside the selected directory
         /// </summary>
         /// <param name="pluginDirectory">Directory containing the plugins to gather info from. Usually BepInEx directory inside game root.</param>
+        /// <param name="subject">Output stream</param>
         private static void TryLoadPlugins(string pluginDirectory, ReplaySubject<PluginInfo> subject)
         {
             Console.WriteLine("Start loading plugins");
@@ -76,10 +74,14 @@ namespace KKManager.Data.Plugins
                     if (Directory.Exists(bep5PluginsDir))
                         files = files.Concat(Directory.EnumerateFiles(bep5PluginsDir, "*.*", SearchOption.AllDirectories));
 
+                    var bep5PatchersDir = Path.Combine(pluginDirectory, "patchers");
+                    if (Directory.Exists(bep5PatchersDir))
+                        files = files.Concat(Directory.EnumerateFiles(bep5PatchersDir, "*.*", SearchOption.AllDirectories));
+
                     token.ThrowIfCancellationRequested();
 
                     var configDir = new DirectoryInfo(Path.Combine(pluginDirectory, "config"));
-                    var configFiles = configDir.Exists ? configDir.GetFiles("*.cfg", SearchOption.TopDirectoryOnly) : new FileInfo[0];
+                    var configFiles = configDir.Exists ? configDir.GetFiles("*.cfg", SearchOption.TopDirectoryOnly) : Array.Empty<FileInfo>();
 
                     foreach (var file in files)
                     {
@@ -145,56 +147,80 @@ namespace KKManager.Data.Plugins
 
             using (var md = ModuleDefinition.ReadModule(dllFile, new ReaderParameters { AssemblyResolver = GetResolver(location) }))
             {
-                var classes = md.Types.Where(x => x.HasCustomAttributes && x.IsClass).ToList();
+                var classes = md.Types.Where(x => x.IsClass).ToList();
 
-                var pluginClasses = classes.Select(c => new
+                var pluginClasses = classes.Where(x => x.HasCustomAttributes).Select(c => new
                 {
                     c,
                     bp = c.CustomAttributes.FirstOrDefault(x => x.AttributeType.FullName == "BepInEx.BepInPlugin")
                 }).Where(x => x.bp != null).ToList();
 
-                if (pluginClasses.Count == 0) yield break;
-
-                var assRefs = md.AssemblyReferences.Select(x => x.FullName).ToArray();
-
-                var f = FileVersionInfo.GetVersionInfo(dllFile);
-                var author = f.CompanyName;
-                var m = Regex.Match(author, @"^https://*.github.com/([^/]+).*?$");
-                if (m.Success) author = m.Groups[1].Value;
-                var description = f.Comments;
-                var fileUrl = new[] { f.CompanyName, f.FileDescription, f.Comments, f.LegalCopyright, f.LegalTrademarks }.FirstOrDefault(x => x.StartsWith("http", StringComparison.OrdinalIgnoreCase));
-
-                foreach (var pc in pluginClasses)
+                if (pluginClasses.Count != 0)
                 {
-                    var depAttributes = pc.c.CustomAttributes
-                        .Where(x => x.AttributeType.FullName == "BepInEx.BepInDependency");
-                    var deps = depAttributes
-                        .Select(x => x.ConstructorArguments.ElementAtOrDefault(0).Value?.ToString())
-                        .Where(x => !string.IsNullOrWhiteSpace(x))
-                        .ToArray();
+                    var assRefs = md.AssemblyReferences.Select(x => x.FullName).ToArray();
 
-                    var url = pc.c.CustomAttributes
-                        .Where(x => x.AttributeType.FullName == "UnityEngine.HelpURLAttribute")
-                        .Select(x => x.ConstructorArguments.ElementAtOrDefault(0).Value?.ToString())
-                        .FirstOrDefault(x => !string.IsNullOrWhiteSpace(x));
-                    if (string.IsNullOrEmpty(url))
-                        url = fileUrl;
+                    var f = FileVersionInfo.GetVersionInfo(dllFile);
+                    var author = f.CompanyName;
+                    var m = Regex.Match(author, @"^https://*.github.com/([^/]+).*?$");
+                    if (m.Success) author = m.Groups[1].Value;
+                    var description = f.Comments;
+                    var fileUrl = new[] { f.CompanyName, f.FileDescription, f.Comments, f.LegalCopyright, f.LegalTrademarks }.FirstOrDefault(x => x.StartsWith("http", StringComparison.OrdinalIgnoreCase));
 
-                    var guid = pc.bp.ConstructorArguments.ElementAtOrDefault(0).Value?.ToString() ?? "Error while loading";
+                    foreach (var pc in pluginClasses)
+                    {
+                        var depAttributes = pc.c.CustomAttributes
+                                              .Where(x => x.AttributeType.FullName == "BepInEx.BepInDependency");
+                        var deps = depAttributes
+                                   .Select(x => x.ConstructorArguments.ElementAtOrDefault(0).Value?.ToString())
+                                   .Where(x => !string.IsNullOrWhiteSpace(x))
+                                   .ToArray();
 
-                    var config = configFiles?.FirstOrDefault(x => string.Equals(x.Name.Substring(0, x.Name.Length - 4), guid, StringComparison.Ordinal));
+                        var url = pc.c.CustomAttributes
+                                    .Where(x => x.AttributeType.FullName == "UnityEngine.HelpURLAttribute")
+                                    .Select(x => x.ConstructorArguments.ElementAtOrDefault(0).Value?.ToString())
+                                    .FirstOrDefault(x => !string.IsNullOrWhiteSpace(x));
+                        if (string.IsNullOrEmpty(url))
+                            url = fileUrl;
 
-                    yield return new PluginInfo(
-                        pc.bp.ConstructorArguments.ElementAtOrDefault(1).Value?.ToString() ?? location.Name,
-                        pc.bp.ConstructorArguments.ElementAtOrDefault(2).Value?.ToString() ?? "Error while loading",
-                        guid,
-                        location,
-                        deps,
-                        assRefs,
-                        author,
-                        description,
-                        url,
-                        config);
+                        var guid = pc.bp.ConstructorArguments.ElementAtOrDefault(0).Value?.ToString() ?? "Error while loading";
+
+                        var config = configFiles?.FirstOrDefault(x => string.Equals(x.Name.Substring(0, x.Name.Length - 4), guid, StringComparison.Ordinal));
+
+                        yield return new PluginInfo(
+                            pc.bp.ConstructorArguments.ElementAtOrDefault(1).Value?.ToString() ?? location.Name,
+                            pc.bp.ConstructorArguments.ElementAtOrDefault(2).Value?.ToString() ?? "Error while loading",
+                            guid,
+                            location,
+                            deps,
+                            assRefs,
+                            author,
+                            description,
+                            url,
+                            config);
+                    }
+                }
+                else if (md.AssemblyReferences.Any(x => x.Name == "Mono.Cecil"))
+                {
+                    // This could be a patcher
+                    var patcherClasses = classes.Any(x =>
+                    {
+                        return x.Methods.Any(m => m.IsStatic && m.Name == "Patch" && m.Parameters.Count == 1) && x.Properties.Any(p => p.Name == "TargetDLLs" && p.GetMethod.IsStatic);
+                    });
+
+                    if (patcherClasses)
+                    {
+                        var f = FileVersionInfo.GetVersionInfo(dllFile);
+                        var author = f.CompanyName;
+                        var m = Regex.Match(author, @"^https://*.github.com/([^/]+).*?$");
+                        if (m.Success) author = m.Groups[1].Value;
+                        var description = f.Comments;
+                        var fileUrl = new[] { f.CompanyName, f.FileDescription, f.Comments, f.LegalCopyright, f.LegalTrademarks }.FirstOrDefault(x => x.StartsWith("http", StringComparison.OrdinalIgnoreCase));
+
+                        var assRefs = md.AssemblyReferences.Select(x => x.FullName).ToArray();
+
+                        yield return new PluginInfo(Path.GetFileNameWithoutExtension(dllFile), string.IsNullOrWhiteSpace(f.FileVersion) ? f.ProductVersion : f.FileVersion, "< PATCHER PLUGIN >", location,
+                                                    Array.Empty<string>(), assRefs, author, description, fileUrl, null);
+                    }
                 }
             }
         }
