8c50730b384ba7fb73c3ad17c1d1273783c599c8
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2023-02-06 02:19:52 +0100

    Update release script

--- src/make_release.ps1
+++ src/make_release.ps1
@@ -12,4 +12,4 @@ Remove-Item $outfile -ErrorAction Ignore -Force
 
 Remove-Item ($dir + "ModpackTool\Temp") -ErrorAction Ignore -Force -Recurse
 
-Get-ChildItem $dir -Exclude *.log,*.pdb,*.xml,*.zip,*.7z,*.ps1,*.lnk,*.settings,FileContentsHashCache.bin,UpdateSourcesDeb* | Compress-Archive -DestinationPath $outfile -Force -CompressionLevel Optimal
+Get-ChildItem $dir -Exclude *.log,*.pdb,*.xml,*.zip,*.7z,*.ps1,*.lnk,*.settings,FileContentsHashCache.bin,UpdateSourcesDeb*,cache | Compress-Archive -DestinationPath $outfile -Force -CompressionLevel Optimal
