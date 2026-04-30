d28b31b385dd9be6a8f1725eb6b78453a345f98b
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2026-04-30 19:39:55 +0200

    Add support for game log files saved inside AppData

--- src/KKManager.Core/Util/PathTools.cs
+++ src/KKManager.Core/Util/PathTools.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 
 namespace KKManager.Util
@@ -136,5 +137,31 @@ namespace KKManager.Util
             }
             return new string(a);
         }
+
+        public static string GetAppDataLocalLowPath()
+        {
+            Guid localLowId = new Guid("A520A1A4-1780-4FF6-BD18-167343C5AF16");
+            return GetKnownFolderPath(localLowId);
+        }
+
+        private static string GetKnownFolderPath(Guid knownFolderId)
+        {
+            IntPtr pszPath = IntPtr.Zero;
+            try
+            {
+                int hr = SHGetKnownFolderPath(knownFolderId, 0, IntPtr.Zero, out pszPath);
+                if (hr >= 0)
+                    return Marshal.PtrToStringAuto(pszPath);
+                throw Marshal.GetExceptionForHR(hr);
+            }
+            finally
+            {
+                if (pszPath != IntPtr.Zero)
+                    Marshal.FreeCoTaskMem(pszPath);
+            }
+        }
+
+        [DllImport("shell32.dll")]
+        private static extern int SHGetKnownFolderPath( [MarshalAs(UnmanagedType.LPStruct)] Guid rfid, uint dwFlags, IntPtr hToken, out IntPtr pszPath);
     }
 }
